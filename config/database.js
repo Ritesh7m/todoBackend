f82d27b1b1994f7a1aadcf7d3fcd5004366d0823
Ritesh7m <maurya.ritesh2005@gmail.com>
2025-03-11 14:01:59 +0530

    second commit

--- config/database.js
+++ config/database.js
@@ -1,16 +1,17 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-const dbConnect =()=>{
-    mongoose.connect(process.env.DATABASE_URL,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true,
+const mongoose = require("mongoose");
+require("dotenv").config();
+const dbConnect = () => {
+  mongoose
+    .connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
-    .then(()=>{
-        console.log('Database connected');
+    .then(() => {
+      console.log("Database connected succesfully");
     })
-    .catch((error)=>{
-        console.log('Error connecting to database',error);
-        process.exit(1); 
-    })
-}
-module.exports=dbConnect;
+    .catch((error) => {
+      console.log("Error connecting to database", error);
+      process.exit(1);
+    });
+};
+module.exports = dbConnect;
